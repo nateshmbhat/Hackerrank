3292dea516abae22351e348eb01198a9859e576c
nateshmbhat <nateshmbhat1@gmail.com>
2017-12-25 06:29:17 +0530

    added ACM problem

--- ACM_ICPC_Team.py
+++ ACM_ICPC_Team.py
@@ -1,2 +1,18 @@
 # https://www.hackerrank.com/challenges/acm-icpc-team/problem
 
+topics = [] 
+topiclists = []
+for a_0 in range(int(input().split()[0])):
+    topics.append(int(input() , 2)) ;
+
+for i in range(len(topics)):
+    for j in range(i+1 , len(topics)):
+        topiclists.append(bin((topics[i] | topics[j])).count('1'));
+ 
+
+
+maxtopic = max(topiclists) ; 
+numofteams = topiclists.count(maxtopic) ; 
+print(maxtopic , numofteams , sep='\n') ;
+
+
